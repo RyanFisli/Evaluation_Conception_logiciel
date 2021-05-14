de1f48eb3217bcf672ee2b477f76eb91a11d4232
RyanFisli <fisliryan@gmail.com>
2021-05-14 15:26:46 +0200

    Creer Deck

--- Serveur/main.py
+++ Serveur/main.py
@@ -4,3 +4,21 @@ from fastapi.responses import JSONResponse
 import requests
 
 app = FastAPI()
+
+id = ""
+
+def Creation_Deck():
+    global id
+    deck = requests.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+    json = deck.json()
+    id = json["deck_id"]
+
+
+@app.get("/")
+def readRoot():
+    return {"API : https://deckofcardsapi.com/"}
+
+@app.get("/creer-un-deck")
+def Creer_Deck():
+    Creation_Deck()
+    return {"deck_id":id}
